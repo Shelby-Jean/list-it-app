f1090686683eddd947676860b4684ea5679205bd
Shelby-Jean <shelby.gottschall@gmail.com>
2021-01-20 18:18:21 -0500

    login page setup

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,7 +1,13 @@
+import '../css/LogIn.css';
+
 const LogIn = () => {
   return (
     <div>
-      <h1>LogIn</h1>
+      <form className="login-form">
+        <input className="login-input" type="text" placeholder="username" />
+        <input className="login-input"  type="password" placeholder="password" />
+        <button className="login-button" type="submit">LOGIN</button>
+      </form>
     </div>
   );
 }
--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,7 +1,10 @@
+import ListItLogo from '../media/List-It-Logo.png';
+import '../css/Navigation.css';
+
 const Navigation = () => {
   return (
-    <div>
-      <h1>Navigation</h1>
+    <div className="nav">
+      <img id="main-logo" src={ListItLogo} alt="List It logo" />
     </div>
   );
 }
