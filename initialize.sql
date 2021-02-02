7218d46820da84648962e3e9f715975242090167
Shelby-Jean <shelby.gottschall@gmail.com>
2021-02-02 17:00:58 -0500

    updated sql tables

--- initialize.sql
+++ initialize.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS users, items, sublists, lists;
+DROP TABLE IF EXISTS users, items, categories, item_category, lists;
 
 CREATE TABLE users (
     user_id INT NOT NULL AUTO_INCREMENT,
@@ -11,26 +11,25 @@ CREATE TABLE items (
     item_id INT NOT NULL AUTO_INCREMENT,
     item_name VARCHAR(45),
     quantity INT,
-    sublist_id INT,
-    PRIMARY KEY (item_id),
-    FOREIGN KEY (sublist_id)
-    REFERENCES sublists (sublist_id)
+    PRIMARY KEY (item_id)
 );
 
-CREATE TABLE sublists (
-    sublist_id INT NOT NULL AUTO_INCREMENT,
-    sublist_name VARCHAR(100),
-    list_id INT,
-    PRIMARY KEY (item_id),
-    FOREIGN KEY (list_id)
-    REFERENCES lists (list_id)
+CREATE TABLE categories (
+    category_id INT NOT NULL AUTO_INCREMENT,
+    catgeory_name VARCHAR(100),
+    PRIMARY KEY (category_id),
+)
+
+CREATE TABLE item_category (
+    category_id INT,
+    item_id INT,
+    PRIMARY KEY (category_id),
+    PRIMARY KEY (item_id)
 )
 
 CREATE TABLE lists (
     list_id INT NOT NULL AUTO_INCREMENT,
-    user_id INT NOT NULL,
-    PRIMARY KEY (list_id),
-    FOREIGN KEY (user_id)
-    REFERENCES users (user_id)
+    list_name VARCHAR(100),
+    PRIMARY KEY (list_id)
 );
 
