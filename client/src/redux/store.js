93fe5be1f1347e5e517deba48fa97deec1961b4f
Shelby-Jean <shelby.gottschall@gmail.com>
2021-04-14 19:11:40 -0400

    trying something with redux dev tools for production

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,10 +1,10 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import reducers from './reducers';
 import state from './state';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 
 
-const store = createStore(reducers, state, composeWithDevTools(applyMiddleware(thunk)));
+const store = createStore(reducers, state, compose(applyMiddleware(thunk), composeWithDevTools()));
 
 export default store;
